534aa141fde18de0857f408d19d93a3cfe8f524f
elia renzoni <elia.renzoni03@gmail.com>
2024-08-05 10:52:53 +0200

    add few new sources

--- internals/app/vector_clock.go
+++ internals/app/vector_clock.go
@@ -1,1 +1,25 @@
 package app
+
+import (
+	"sync"
+	tt "vectorclock/transport"
+)
+
+type VectorClock struct {
+	vector map[int]tt.Peers
+	mutex  sync.Mutex
+}
+
+func NewVectorClock() *VectorClock {
+	return &VectorClock{
+		vector: make(map[int]tt.Peers),
+	}
+}
+
+func (v *VectorClock) UpdateLocalClock(host string, port int) {
+
+}
+
+func (v *VectorClock) MergeVectorClocks(senderVectorClock map[int]tt.Peers) {
+
+}
--- internals/transport/peers.go
+++ internals/transport/peers.go
@@ -1,1 +1,22 @@
 package transport
+
+type Peers struct {
+	host       string
+	port       int
+	nodeID     int
+	localClock int
+}
+
+func NewPeers() *Peers {
+	return &Peers{
+		// empty one
+	}
+}
+
+func (p *Peers) AddPeer(host string, port, nodeID int) {
+
+}
+
+func (p *Peers) SearchPeer(host string, port int) bool {
+	return false
+}
